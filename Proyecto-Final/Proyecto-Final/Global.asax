7ae0605dbb690bad4b2063a691738fc0be238707
fercho-05 <fperezalpizar@gmail.com>
2024-01-21 18:55:22 -0600

    Correcciones

--- Proyecto-Final/Proyecto-Final/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Final.MvcApplication" Language="C#" %>
---
+++ SAEE_API/SAEE_API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SAEE_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
