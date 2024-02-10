ccff3b4578859875a218ad3faf9160e99d980606
Sebasrr2903 <105829356+Sebasrr2903@users.noreply.github.com>
2024-02-10 00:44:40 -0600

    HISTORIA USUARIO RS-03 LISTO

---
+++ SAEE_API/SAEE_API/Controllers/CoursesController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SAEE_API;
+using SAEE_API.Entities;
+
+namespace SAEE_API.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        //To insert into errors and actions table
+        Reports reports = new Reports();
+
+        /***********************Cuorses List***********************/
+        [HttpGet]
+        [Route("CoursesList")]
+        public List<Courses> CoursesList()
+        {
+            try
+            {
+                using (var context = new SAEEEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    return (from x in context.Courses
+                            select x).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                string errorDescription = e.Message.ToString();
+                reports.ErrorReport(errorDescription, 1, "CoursesList");
+
+                return new List<Courses>();
+            }
+        }
+
+
+
+        [HttpPut]
+        [Route("ChangeStatusCourses")]
+        public string ChangeStatusCourse(CoursesEnt course)
+        {
+            try
+            {
+                using (var context = new SAEEEntities())
+                {
+                    var data = (from x in context.Courses
+                                where x.id == course.Id
+                                select x).FirstOrDefault();
+
+                    if (data != null)
+                    {
+                        data.active = (data.active == true ? false : true);
+                        context.SaveChanges();
+                    }
+
+                    reports.ActionReport("ChangeStatusCoursesDone",course.activeUser, "ChangeStatusCourses");
+                    return "OK";
+                }
+            }
+            catch (Exception e)
+            {
+                string errorDescription = e.Message.ToString();
+                reports.ErrorReport(errorDescription, 0, "ChangeStatusCourses");
+
+                return string.Empty;
+            }
+        }
+
+
+
+
+        [HttpGet]
+        [Route("CourseData")]
+        public Courses CourseData(long q)
+        {
+            try
+            {
+                using (var context = new SAEEEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    var datos = (from x in context.Courses
+                                 where x.id == q
+                                 select x).FirstOrDefault();
+
+                    return datos;
+                }
+            }
+            catch (Exception e)
+            {
+                string errorDescription = e.Message.ToString();
+                reports.ErrorReport(errorDescription, 0, "CourseData");
+
+                return null;
+            }
+        }
+        [HttpPut]
+        [Route("UpdateCourse")]
+        public string UpdateCourse(CoursesEnt course)
+        {
+            try
+            {
+                using (var context = new SAEEEntities())
+                {
+                    var data = (from x in context.Courses
+                                where x.id == course.Id
+                                select x).FirstOrDefault();
+
+                    if (data != null)
+                    {
+                        //De momento se hace por Linq
+                        data.name = course.Name;
+                        data.description = course.Description;
+                        data.availableQuota = course.AvailableQuota;
+                        data.active = course.Active;
+                  
+
+                        context.SaveChanges();
+                    }
+
+                    reports.ActionReport("UpdateCourseDone", course.activeUser, "UpdateCourse");
+                    return "OK";
+                }
+            }
+            catch (Exception e)
+            {
+                string errorDescription = e.Message.ToString();
+                reports.ErrorReport(errorDescription, 0, "UpdateCourse");
+
+                return string.Empty;
+            }
+        }
+
+
+        [HttpPost]
+        [Route("RegisterCourse")]
+        public string RegisterCourse(CoursesEnt course)
+        {
+              try
+            {
+                using (var context = new SAEEEntities())
+                {
+                    var courses = new Courses();
+                    courses.description = course.Description;
+                    courses.active = course.Active;
+                    courses.name = course.Name; 
+                    courses.availableQuota = course.AvailableQuota;
+
+                    context.Courses.Add(courses);
+                    context.SaveChanges();
+
+                    return "OK";
+                }
+            }
+            catch (Exception e)
+            {
+                string errorDescription = e.Message.ToString();
+                reports.ErrorReport(errorDescription, course.activeUser, "RegisterCourse");
+
+                return string.Empty;
+            }
+
+        }
+    }
+}
--- SAEE_Web/SAEE_Web/Controllers/CourseController.cs
+++ SAEE_Web/SAEE_Web/Controllers/CourseController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SAEE_Web.Entities;
+using SAEE_Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,107 @@ namespace SAEE_Web.Controllers
 {
     public class CourseController : Controller
     {
+
+        CourseModel coursesModel =  new CourseModel();
+
+
+
+
         [HttpGet]
         public ActionResult AllCourses()
         {
             return View();
         }
+
+
+        /**********************TABLE/STATUS/UPDATE/RECOVER**********************/
+        [HttpGet]
+        public ActionResult CoursesTable()
+        {
+            int ActiveId = (int)Session["ActiveId"];
+            var datos = coursesModel.CoursesList().Where(x => x.Id != ActiveId).ToList();
+            return View(datos);
+        }
+
+        [HttpGet]
+        public ActionResult ChangeStatusCourse(long q)
+        {
+            var course = new  CoursesEnt();
+            course.Id = (int)q;
+
+            course.activeUser = (int)Session["ActiveId"];//For action register
+
+            var resp = coursesModel.ChangeStatusCourse(course);
+
+            if (resp == "OK")
+            {
+                return RedirectToAction("CoursesTable", "Course");
+            }
+            else
+            {
+                ViewBag.BoxMessage = "No se pudo modificar el estado del curso.";
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult RegisterCourse()
+        {
+      
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterCourse(CoursesEnt course)
+        {
+            course.activeUser = (int)Session["ActiveId"];//For action register
+
+            ViewBag.CourseName = course.Name;
+
+            var resp = coursesModel.RegisterCourse(course);
+
+            if (resp == "OK")
+            {
+                return RedirectToAction("CoursesTable", "Course");
+            }
+            else
+            {
+                ViewBag.BoxMessage = "No se ha registrado el curso.";
+               
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult UpdateCourse(long q)
+        {
+            var data = coursesModel.CourseData(q);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCourse(CoursesEnt course)
+        {
+            course.activeUser = (int)Session["ActiveId"];//For action register
+
+            var resp = coursesModel.UpdateCourse(course);
+
+            if (resp == "OK")
+            {
+                return RedirectToAction("CoursesTable", "Course");
+            }
+            else
+            {
+                ViewBag.BoxMessage = "No se pudo actualizar el usuario.";
+                return View();
+            }
+        }
+
+
+
+
 
         [HttpGet]
         public ActionResult SpecificCourse()
